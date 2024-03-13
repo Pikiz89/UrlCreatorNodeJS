eebab9c9f9ada749a552308e68f0360db0ec24b7
Pikiz89 <140271527+Pikiz89@users.noreply.github.com>
2024-03-13 16:26:06 +0100

    Update server.JS --Added a function generateUrl

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const http = require("http");
 const port = 8080;
 const fs = require("fs");
 const server = http.createServer();
+const urls=[]
 
 server.on("request", (req,res)=>{
-    res.end("hello, world !")
+    if(req.url === "/create"){
+        let generatedUrl = generateUrl();
+        res.end(`${generatedUrl} is your new URL !`);
+    }
 })
 server.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
 })
+function generateUrl() {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let generatedUrl ="";
+    for(i = 0; i<3; i++){
+        let randInt = Math.floor(Math.random()*26);
+        generatedUrl += alphabet[randInt];
+    }
+    console.log(`New URL = ${generatedUrl}`);
+    return generatedUrl;
+}
